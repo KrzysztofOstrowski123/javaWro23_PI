e0a84482425c0110b3f94446fca333af6f19a5f4
KrzysztofOstrowski123 <ostry667123@gmail.com>
2019-08-10 10:20:14 +0200

    PersonApp

---
+++ src/main/java/PersonApp/Person.java
@@ -0,0 +1,29 @@
+package PersonApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+
+    private List<Language> languages;
+
+
+    public Person(Language language) {
+        languages = new ArrayList<Language>();
+        languages.add(language);
+
+    }
+
+    public void learn(Language language) {
+        languages.add(language);
+
+    }
+
+    public void sayHello() {
+        for (Language lang:languages)
+            System.out.println(lang.hello() + "   ");
+        System.out.println();
+
+    }
+}
