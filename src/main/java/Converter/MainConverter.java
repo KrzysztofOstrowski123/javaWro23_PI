b976757fd48a533d088e2c01de6738cef428b7ea
KrzysztofOstrowski123 <ostry667123@gmail.com>
2019-08-10 11:22:13 +0200

    converter base implementation

---
+++ src/main/java/Converter/MainConverter.java
@@ -0,0 +1,8 @@
+package Converter;
+
+public class MainConverter {
+    public static void main(String[] args) {
+        NumbersConverter numbersConverter = new NumbersConverter();
+        System.out.println(numbersConverter.binaryDecimal("00101010"));
+    }
+}
